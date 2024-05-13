d4adefef3a78be99d353842df8593a74b8395c45
Ripe-melon <167770152+Ripe-melon@users.noreply.github.com>
2024-05-13 22:14:51 +0200

    small fix, now work

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,9 @@ function Home() {
     <>
       <div className="home">
         <h1>React Navbar</h1>
-        <h3>Difficulty Level: 0;</h3>
+        <h3>Difficulty Level: 69;</h3>
+
+
       </div>
     </>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,2 +1,24 @@
-import { Component } from "react";
+import { useState } from "react";
 import "./NavbarStyles.css";
+
+function Navbar() {
+    // Initialize the count state variable with 0
+    const [count, setCount] = useState(0);
+
+    // Function to increment the count
+    const handleIncrement = () => {
+        setCount(count + 1);
+    };
+
+    return (
+        <>
+            <div className="home">
+                <h1>React Navbar</h1>
+                <h3>Button count = {count}</h3>
+                <button onClick={handleIncrement}>Increment</button>
+            </div>
+        </>
+    );
+}
+
+export default Navbar;
